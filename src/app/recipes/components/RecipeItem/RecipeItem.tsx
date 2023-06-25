52d0fcf254e6f541371c50fdaf6f89415a117dc2
Kornelijus Semėnas <121442495+Ka0gaMi@users.noreply.github.com>
2023-06-25 23:52:05 +0300

    RecipeItem init

---
+++ src/app/recipes/components/RecipeItem/RecipeItem.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./RecipeItem.module.css";
+
+export default function RecipeItem() {
+  return (
+    <>
+      <div className={styles.RecipeItemContainer}>
+        Hi
+      </div>
+    </>
+  )
+}
